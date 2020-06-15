e9d196f6195f310da31fe1a12a8fd2282c9fe891
Patrick Ding <pd_35_delancy@yahoo.com>
2020-06-14 19:52:45 -0500

    more stuff

---
+++ src/angle_sampler.cpp
@@ -0,0 +1,20 @@
+#include "angle_sampler.h"
+
+//' Compute the cumulatve lengths of the slices, with a zero prepended 
+//' :return: cumulative lengths of the slices
+arma::vec AngleSampler::get_slices_cumulative_length() {
+  arma::vec lengths = this->rotated_slices.col(1) - this->rotated_slices.col(0);
+  arma::vec cum_len = arma::cumsum(lengths);
+  cum_len.insert_rows(0, 1);
+  return cum_len;
+}
+
+//' Draw one sample angle from the given slice
+//' :return: random angle from slice(s)
+double AngleSampler::draw_angle() {
+  arma::vec cum_len = this->get_slices_cumulative_length();
+  double l = cum_len(cum_len.n_elem - 1);
+  
+  double sample = l * arma::randu();
+  int idx = arma::find()
+}
--- src/angle_sampler.h
+++ src/angle_sampler.h
@@ -4,18 +4,20 @@
 #include "active_intersections.h"
 
 class AngleSampler {
-public
   ActiveIntersections active_intersections;
   double rotation_angle;
-  arma::vec rotated_slices;
+  arma::mat rotated_slices;
   
   AngleSampler(ActiveIntersections active_intersections) :
     active_intersections(active_intersections) {
-    std::pair<double, arma::vec> 
+    std::pair<double, arma::vec> result = active_intersections.rotated_intersections();
+    rotation_angle = result.first;
+    rotated_slices = arma::reshape(result.second, result.second.size() / 2, 2);
   }
+  
+  arma::vec get_slices_cumulative_length();
+  double draw_angle();
 };
-
-
 
 #endif ANGLESAMPLER_H
 
