e6c03b8ee33215eedaf60248a3c39a300e1b0528
Patrick <patrickding00@gmail.com>
2020-08-15 21:55:04 -0500

    fix some bugs, proper constraint handling

--- R/lcg_tmvn.R
+++ R/lcg_tmvn.R
@@ -1,10 +1,28 @@
+get_polytope_constraints <- function(lb, ub, mu, U) {
+  d <- length(lb)
+  
+  inf_idx <- c(is.infinite(lb), is.infinite(ub))
+  
+  A <- rbind(diag(d), -diag(d))[!inf_idx, ]
+  A <- A %*% U
+  
+  b <- c(mu - lb, -mu + ub)[!inf_idx]
+  
+  return(list(A = A, b = b))
+}
+
 #' @export
 rtmvn <- function(n, mu, Sigma, lb, ub, x_init = NULL,
                   mode = "intersection", verbose = FALSE) {
-  U <- chol(Sigma)
-  d <- length(mu)
-  A <- rbind(U, -U)
-  b <- c(mu - lb, -mu + ub)
+  # U <- chol(Sigma)
+  L <- t(chol(Sigma))
+  # d <- length(mu)
+  # A <- rbind(U, -U)
+  # b <- c(mu - lb, -mu + ub)
+  
+  pc <- get_polytope_constraints(lb, ub, mu, L)
+  A <- pc$A
+  b <- pc$b
   
   if (mode == "intersection")
     mode_bool = TRUE
@@ -18,18 +36,24 @@ rtmvn <- function(n, mu, Sigma, lb, ub, x_init = NULL,
   }
   
   std_samples <- t(ess(n, A, b, x_init, mode_bool, verbose))
-  samples <- std_samples %*% t(U) + matrix(rep(mu, n), nrow = n, byrow = TRUE)
+  # samples <- std_samples %*% t(U) + matrix(rep(mu, n), nrow = n, byrow = TRUE)
+  samples <- std_samples %*% t(L) + matrix(rep(mu, n), nrow = n, byrow = TRUE)
   return(samples)
 }
 
+#' @export
 ptmvn <- function(mu, Sigma, lb, ub, mode = "intersection",
                   n_sub_samples = 10, domain_fraction = .5, n_sub_skip = 0,
                   n_hdr_samples = 10, n_hdr_skip = 0,
                   log = FALSE) {
-  U <- chol(Sigma)
-  d <- length(mu)
-  A <- rbind(U, -U)
-  b <- c(mu - lb, -mu + ub)
+  L <- t(chol(Sigma))
+  # d <- length(mu)
+  # A <- rbind(U, -U)
+  # b <- c(mu - lb, -mu + ub)
+  
+  pc <- get_polytope_constraints(lb, ub, mu, L)
+  A <- pc$A
+  b <- pc$b
   
   if (mode == "intersection")
     mode_bool = TRUE
